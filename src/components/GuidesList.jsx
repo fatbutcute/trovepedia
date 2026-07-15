0eb1f2126cda7b4fe88aac85ff814d7f7e154f97
Gábor <vargaarongabor@gmail.com>
2026-07-15 17:23:00 +0200

    Max Light guide added

--- src/components/GuidesList.jsx
+++ src/components/GuidesList.jsx
@@ -87,7 +87,170 @@ If you wish to see the best build for your class, check out this spreadsheet:
     title: 'Maximize Your Light', 
     desc: 'This guide shows you how to maximize your light.', 
     icon: 'ri-sun-line', 
-    slug: 'light-system' 
+    slug: 'light-system',
+    bgImage: '/guidebg/cosmic.webp',
+    author: 'Mortrak, Losiqn - Arsyn',
+    date: '2026-07-15',
+    content: `## How to get max light?
+
+This section covers the optimal setup for endgame progression, focusing heavily on maximizing your Light stat to deal with high-tier enemies.
+
+
+
+## Empowered Cosmic Gem (Berserk Battler) &amp; Lesser Cosmic Gem x2
+
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTI_GEMS3page0002.webp" alt="Optimal Cosmic Gems Setup" />
+</div>  
+
+These are max mystic gems. Make sure they have **3 boosts*** on Light and are **100% augmented**. The *Berserk Battler* Empowered Gem gives an additional <span class="text-cyan">+750 Light</span> if it’s fully procced.
+
+### Total: <span class="text-cyan">4945 Light</span> <span class="text-gold">(+750 from Berserk Battler)</span>
+
+<div class="gd-info-box">
+  <div class="gd-info-box-content">
+    <h4 class="gd-info-title">What are Gem Boosts?</h4>
+    <p>
+      These boosts are represented by the <strong>blue pearls</strong> above the stat in the Gem Forge. Max gems have exactly 3 boosts. Empowered gems must reach 3 boosts by <strong>Level 15</strong> - if they don't, they are 2-star gems and should be replaced immediately.
+    </p>
+  </div>
+</div>
+
+
+
+## C5 Gear - Total Light: 4160
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5hatmax.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5weaponmax.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5facemax.png"/>
+</div>
+
+### Upgrade Cost per Piece
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTICGEMS3page0003.webp"/>
+</div>
+
+## Mystic Gear - Total Light: 4680
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Mystichatmax.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Mysticweaponmax.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Mysticfacemax.png"/>
+</div>
+
+## Misc. Gear - Total Light: 1675
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTICGEMS3page0005.webp"/>
+</div>
+
+### Banner, Rings and Auto-Use Rune:
+* Enshadowed Torch of Knotted Shadow: One of the necessary items to reach max light. It can be obtained from Geode Topside Leviathan bosses. Note: Banners such as the Enshadowed Banner of Knotted Shadow cannot be traded and there are several type of banners which gives different stat bonuses. Good to know: You have a guarenteed chance to get the first banner after killing 250 leviathans.
+* Rings: For long time, only crystal rings were available, but now you can also get the new mystic rings. Both of them are good, but mystic rings are better because they gives more light and new ring abilities. Mystic rings can be crafted at the Ringcrafting Bench after reaching max lvl in the Ringcrafter Bench. To be able to Mystic Rings you need mystic loops which are similar to primal loops, but mystic loops can be only acquired from D15, after reaching a new primal level. Note: You must be in D15 in the moment when you get the new primal level.
+* Auto-Use Rune: This rune is a must-have for endgame players. Can be obtainable from the Rune Crafting Bench, after reaching a specified level in the bench.
+
+## Allys - Total Light: 400 or 500
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTICGEMS3page0006.webp"/>
+</div>
+
+### 
+* Scorpius: One of the allies that gives a moderate amount of light. It can be obtained from the Star Chart. Very good for classes which have a lot of mobility such as Shadow Hunter. One of the best allies for farming.
+* Orchian: Similarly as Scorpius, Orchian is a good ally for classes with magic damage and high mobility. It can be obtained from the Paragon Workbench.
+
+## Dragons - Total Light: 570
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTICGEMS3page0007.webp" alt="Optimal Cosmic Gems Setup" />
+</div>
+Each dragon gives a significant amount of light, and the total light from dragons can reach up to 570. This amount can be achieved if you have max mystic cosmic gems because the cosmic dragon scales from your cosmic gems.
+
+## Star Chart - Total Light: 250
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTICGEMS3page0008.webp"/>
+</div>
+
+##
+* Star Chart: There are some orbs in the Star Chart (as shown on the image) which gives small amounts of light. Two of the 3 orbs unlocks Scorpius and Starry Skyfire allies.
+* Constellation Key: Count this item as some kind of currency. If you have Constellation Keys, you can unlock minor orbs in the Star Chart. Constellation Keys can be either acquired for 1300 cubits on the Store -> More tab or you can obtain the Locksmith's Lexicon by reaching Mastery Lvl 270. The tome gives you 1 key every week. You need 91 keys to unlock all the 3 orbs which is 118.300 cubits.
+* Celestial Sphere: Used to unlock major orbs in the Star Chart. You can obtain these spheres at the Celestial in the NW building of the Hub. Note: The first 3 Celestial Sphere costs 100 flux each, after buying the first amounts, you will need Astral Echoes in the future to buy more Celestial Spheres.
+* Astral Echoes: Currency to buy Celestial Spheres. Best ways to get Astral Echoes: 
+
+  * Farming dungeons, chest will drop different amounts of Astral Echoes every 5 minutes. After 5000 per week, the time drops to 15 minutes (this resets every week on Monday).
+  * Buying the Astral Echoes Almanac tome (Store -> More tab): Gives 5000 Astral Echoes every week.
+
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Celestial.webp"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Celestial1.webp"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Celestial2.webp"/>
+</div>
+
+## Other - Total Light: 1140 Light + 1% of overall total
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/C5GEARMYSTICGEMS3page0009.webp"/>
+</div>
+
+### Why are these important?
+* Solarion Subclass: Like any subclass works, Solarion's subclass gives some amount of light. The amount of light depends on the subclass level. In order to get the 140 light, Solarion must reach lvl 30.
+* Geode Mastery: Geode mastery is a must do part of the endgame. Every level gives 10 light to your current light, which is 1000 light at lvl 100.
+* Litany: This buff gives 1% light of your current light. In order to craft this buff, you must go to the Altar of Martial Artistry bench (it's located SW on the hub) and craft the Mantra of Regeneration. When you crafted the Mantra of Regeneration, you must buy a Quality License from Disciple Daniel. When you have the Quality License, go to the Relic of Preservation and craft the Litany buff. This buff lasts until next weekly reset.
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/AltarofMartialArtistry.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/AltarofMartialArtistry2.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Discipledaniel.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Discipledaniel2.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Relicofpreservation.png"/>
+</div>
+<div class="gd-image-container">
+  <div class="gd-image-glow-border"></div>
+  <img src="/guideimages/Relicofpreservation2.png"/>
+</div>
+
+## And now everything added up!
+<span classname="crystalgear">C5 Gear: 13271 Light - using 400 light Ally.</span>\n
+<span classname="mysticgear">Mystic Gear: 13796 Light - using 400 light Ally.</span>\n
+###
+###\n\n
+<span classname="light">Absolute Max Light can be 14604 - with (450 Light) Ally + Berserk Battler (we count the Berserk Battler's proc as plus light.)</span>
+`
   },
   { 
     tag: 'Leviathans and Torches', 
