6fafe7c5a1b4be35ba7a7823ed2b2785f28e45d0
Gábor <vargaarongabor@gmail.com>
2026-07-30 14:13:50 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ import GuideDetailPage from './pages/GuideDetailPage';
 import TroveNewsPage from './components/TroveNewsPage';
 import FishingPage from './pages/FishingPage';
 import CalculatorsPage from './components/CalculatorsPage';
-import TokenCall from './components/tokencall';
-import TokenCallPage from './pages/TokenCallPage';
+import TokenCall from './components/TokenCall';
 
 export default function App() {
   return (
@@ -52,7 +51,7 @@ export default function App() {
 
           <Route path="/calculators" element={<CalculatorsPage />} />
 
-          <Route path="/token-test" element={<TokenCallPage />} />
+          <Route path="/token-test" element={<TokenCall />} />
 
           <Route
             path="/fishing"
