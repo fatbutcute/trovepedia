bcc4bf4efef4672554eff3b6d96753f2b1e7e92a
Gábor <vargaarongabor@gmail.com>
2026-09-20 21:31:28 +0200

    Update Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import ClubsPage       from './pages/ClubsPage'
 import TroveNewsPage   from './components/TroveNewsPage'
 import FishingPage     from './pages/FishingPage'
 import CalculatorsPage from './components/CalculatorsPage'
-import TokenCall       from './components/tokencall'
+import TokenCall       from './components/TokenCall'
 import Leaderboard from './components/Leaderboard'; // 👈 IMPORTÁLÁS
 
 import GuideViewer     from './components/guides/GuideViewer'
